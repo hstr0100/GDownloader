f21872111cab6c7ea2a5c52f1d934fa4a1bc544f
hstr0100 <laertztxd08@gmail.com>
2025-03-01 00:33:29 -0300

    Add additional audio bitrate options

--- core/src/main/java/net/brlns/gdownloader/settings/enums/AudioBitrateEnum.java
+++ core/src/main/java/net/brlns/gdownloader/settings/enums/AudioBitrateEnum.java
@@ -25,11 +25,22 @@ import static net.brlns.gdownloader.lang.Language.*;
  */
 @Getter
 public enum AudioBitrateEnum implements ISettingsEnum {
-    // TODO some containers can go way higher, account for that
     NO_AUDIO(0, ""),
+    BITRATE_32(32, "32kbps"),
+    BITRATE_64(64, "64kbps"),
+    BITRATE_96(96, "96kbps"),
+    BITRATE_128(128, "128kbps"),
+    BITRATE_160(160, "160kbps"),
     BITRATE_192(192, "192kbps"),
     BITRATE_256(256, "256kbps"),
-    BITRATE_320(320, "320kbps");
+    BITRATE_320(320, "320kbps"),
+    BITRATE_384(384, "384kbps"),
+    BITRATE_448(448, "448kbps"),
+    BITRATE_512(512, "512kbps"),
+    BITRATE_640(640, "640kbps"),
+    BITRATE_768(768, "768kbps"),
+    BITRATE_896(896, "896kbps"),
+    BITRATE_1024(1024, "1024kbps");
 
     private final int value;
     private final String displayName;
